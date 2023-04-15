dfb7e77da8f368f05a10b07d585edd9018d5a166
YanoRyo <yanoryo7032@gmail.com>
2023-04-15 17:45:42 +0900

    アプリタイトル表示テキストの追加

--- app/src/main/java/com/example/bmicalculatorjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculatorjetpackcompose/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.bmicalculatorjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bmicalculatorjetpackcompose.ui.theme.BMICalculatorJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +26,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(20.dp)
+                    ) {
+                        Text(
+                            text = "BMI計算アプリ",
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                    }
                 }
             }
         }
